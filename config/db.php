2a4e3dff14ae39309fdcc6a64a2fa44898fd495f
dpdatadev <91690736+dpdatadev@users.noreply.github.com>
2022-12-28 15:04:52 -0600

    set up heroku postgres

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=lin-12553-3614-pgsql-primary.servers.linodedb.net;port=5432;dbname=webapp01',
-    'username' => 'linpostgres',
-    'password' => 'KHrdU1JRn9H_8EsO',
+    'dsn' => 'pgsql:host=ec2-52-203-118-49.compute-1.amazonaws.com;port=5432;dbname=ddsfrk9olcimk6',
+    'username' => 'lkdiokkyvbuxnp',
+    'password' => 'bd0f8e5a83d18c188139264be052cbdc6a530306701fbe3ccaa4c3dc6f655f10',
     'charset' => 'utf8',
 ];
