ce07ef1bd1ac8e1d6443bccb9e11a5910fdfe179
dpdatadev <91690736+dpdatadev@users.noreply.github.com>
2022-12-28 14:52:27 -0600

    crap forgot db for heroku

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=lin-12553-3614-pgsql-primary.servers.linodedb.net;port=5432;dbname=webapp01',
+    'username' => 'linpostgres',
+    'password' => 'KHrdU1JRn9H_8EsO',
+    'charset' => 'utf8',
+];
--- config/web.php
+++ config/web.php
@@ -1,7 +1,7 @@
 <?php
 
-$params = require __DIR__ . '/params.php';
-$db = require __DIR__ . '/db.php';
+$params = require('params.php');
+$db = require('db.php');
 
 $config = [
     'id' => 'basic',
