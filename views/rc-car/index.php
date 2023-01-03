26791627da602cd8bd40d936d88e116201394b28
dpdatadev <91690736+dpdatadev@users.noreply.github.com>
2023-01-03 08:20:25 -0600

    remove id from display

--- views/rc-car/index.php
+++ views/rc-car/index.php
@@ -49,8 +49,6 @@ $this->params['breadcrumbs'][] = $this->title;
         'filterModel' => $searchModel,
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
-
-            'id',
             'name',
             'make',
             'model',
